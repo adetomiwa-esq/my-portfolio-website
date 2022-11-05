9183e9d62639680f623af365f5a88fff9bdafa84
Akinola Kehinde <khennyakin@gmail.com>
2022-11-06 00:42:06 +0100

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/Pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <Router basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Navbar />
         <Routes>
           <Route path='/my-portfolio-website' element={<Home />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
   );
 }
